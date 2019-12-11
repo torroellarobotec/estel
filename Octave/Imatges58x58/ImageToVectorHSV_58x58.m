0c0125dfc5269b973e2285be6353d1f59e5cf20b
ROBOTEC <info@robotec.cat>
2019-12-11 22:50:13 +0100

    Afegides Imatges 58x58 i nou codi a Octave

---
+++ Octave/Imatges58x58/ImageToVectorHSV_58x58.m
@@ -0,0 +1,42 @@
+clear all;
+pkg load image;
+
+  file = 'Estrella8.png';
+  ImageOrig = imread(file);
+  ImageRGB = imresize(ImageOrig,[58,58]);
+  #ImageRGB = ImageOrig;
+  ImageHSV = rgb2hsv (ImageRGB);
+  
+  vectorHSV = "";
+  Columnes = 58;
+
+  while Columnes >= 1    
+    #Files parelles
+    Files = 58;
+    while Files >= 1
+      H = ImageHSV(Files, Columnes, 1);      
+      S = ImageHSV(Files, Columnes, 2);
+      V = ImageHSV(Files, Columnes, 3);
+    
+      vectorHSV = strcat(vectorHSV, "{", num2str(round(H*255)), "," , num2str(round(S*255)) , "," , num2str(round(V*255)) ,"},");    
+      Files = Files - 1;
+    endwhile
+    
+    Columnes = Columnes - 1;
+    #Files senars
+    Files = 1;
+    while Files <= 58
+      H = ImageHSV(Files, Columnes, 1);
+      S = ImageHSV(Files, Columnes, 2);
+      V = ImageHSV(Files, Columnes, 3);
+      
+      vectorHSV = strcat(vectorHSV, "{", num2str(round(H*255)), "," , num2str(round(S*255)) , "," , num2str(round(V*255)) ,"},");      
+      Files = Files + 1;    
+    endwhile
+    
+    Columnes = Columnes -1;
+  endwhile
+    
+  save Estrella_8_HSV_58x58.txt vectorHSV;
+  
+  imshow(ImageRGB);
